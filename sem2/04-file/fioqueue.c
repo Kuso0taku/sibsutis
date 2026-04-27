ff9cab005628cb69c45ab04269fb2dc9352f5a76
Kuso0taku <denustuzan@gmail.com>
2026-04-27 21:13:47 +0700

    04 binary functions

--- sem2/04-file/fioqueue.c
+++ sem2/04-file/fioqueue.c
@@ -3,29 +3,30 @@
 #include <stdlib.h>
 
 // additional
+static size_t gen_rand_size_t(size_t min, size_t max) {
+  return (size_t)rand() / (RAND_MAX + 1.0) * (max - min) + min;
+}
+
 static void fill_rand_matrix2d(Matrix2d* m) {
-    size_t cols = gen_rand_int(1, MAX);
-    size_t rows = gen_rand_int(1, MAX);
+    size_t rows = gen_rand_size_t(1, MAX_ROW);
+    size_t cols = gen_rand_size_t(1, MAX_COL);
   
     matrix2d_construct(m, rows, cols, NULL);
-    matrix2d_random(m, MIN, MAX);
-
-    return m
+    matrix2d_random(m, MIN_DOUBLE, MAX_DOUBLE);
 }
 
 // Grade "Satisfactory"
 Matrix2d* rand_gen_matrix2d(size_t n) {
-  size_t rows, cols;
   Matrix2d* arr = (Matrix2d*)malloc(n * sizeof(Matrix2d));
 
   for (size_t i=0; i<n; i++) {
-    fill_rand_matrix2d(arr+i)
+    fill_rand_matrix2d(arr+i);
   }
 
   return arr;
 }
 
-int save_queue_to(const Queue* q, const char* filename) {
+int save_text_queue_to(const Queue* q, const char* filename) {
   if (!q) return -1;
   FILE *f = fopen(filename, "w");
   if (!f) return -1;
@@ -48,7 +49,7 @@ int save_queue_to(const Queue* q, const char* filename) {
   return 0;
 }
 
-int load_queue_from(Queue* q, const char* filename) {
+int load_text_queue_from(Queue* q, const char* filename) {
   FILE *f = fopen(filename, "r");
   if (!f) return -1;
 
@@ -103,9 +104,101 @@ Matrix2d* get_text_element(const char* filename, int index) {
 }
 
 // Grade "Good"
-int saveb_queue_to(const Queue*, const char*);
-int loadb_queue_from(Queue*, const char*);
-int get_binary_element(const char*, int, size_t, void*); // get by index
+int save_binary_queue_to(const Queue* q, const char* filename) {
+  FILE* f = fopen(filename, "wb");
+  if (!f) return -1;
+
+  size_t q_size = queue_size(q);
+  fwrite(&q_size, sizeof(size_t), 1, f);
+
+  Iterator cur, end;
+  iter_begin(&cur, q);
+  iter_end(&end, q);
+  
+  while (!iter_equal(&cur, &end)) {
+    Matrix2d* m = (Matrix2d*)iter_get(&cur);
+    fwrite(&m->rows, sizeof(size_t), 1, f);
+    fwrite(&m->cols, sizeof(size_t), 1, f);
+  
+    fwrite(m->data, sizeof(double), m->rows * m->cols, f);
+
+    iter_next(&cur);
+  }
+
+  fclose(f);
+  return 0;
+}
+
+int load_binary_queue_from(Queue* q, const char* filename) {
+  FILE* f = fopen(filename, "rb");
+  if (!f) return -1;
+
+  size_t q_size;
+  fread(&q_size, sizeof(size_t), 1, f);
+  
+  size_t rows, cols;
+  for (size_t i=0; i < q_size; i++) {
+    if (fread(&rows, sizeof(size_t), 1, f) != 1) break;
+    if (fread(&cols, sizeof(size_t), 1, f) != 1) break;
+    
+    Matrix2d* m = matrix2d_construct_default();
+    matrix2d_construct(m, rows, cols, NULL);
+
+    if (fread(m->data, sizeof(double), m->rows * m->cols, f) != rows*cols) {
+      matrix2d_destruct(m);
+      break;
+    }
+    
+    if (m) enqueue(q, m);
+  }
+
+  fclose(f);
+  return 0;
+
+}
+
+int get_binary_element(const char* filename, int index, void* result) {
+  FILE* f = fopen(filename, "rb");
+  if (!f) return -1;
+
+  size_t q_size;
+  if (fread(&q_size, sizeof(size_t), 1, f) != 1) {
+    fclose(f);
+    return -1;
+  }
+
+  if (index < 0 || (size_t)index >= q_size) {
+    fclose(f);
+    return -2;
+  }
+  
+  size_t r, c;
+
+  // skip matrices
+  for (size_t i=0; i < index; i++) {
+    if (fread(&r, sizeof(size_t), 1, f) != 1) { fclose(f); return -1; }
+    if (fread(&c, sizeof(size_t), 1, f) != 1) { fclose(f); return -1; }
+
+    fseek(f, (long)(r*c * sizeof(double)), SEEK_CUR);
+  }
+  
+  // read matrix
+  if (fread(&r, sizeof(size_t), 1, f) != 1) { fclose(f); return -1; }
+  if (fread(&c, sizeof(size_t), 1, f) != 1) { fclose(f); return -1; }
+
+  Matrix2d* m = matrix2d_construct_default();
+  matrix2d_construct(m, r, c, NULL);
+  
+  if (fread(m->data, sizeof(double), r*c, f) != r*c) {
+    matrix2d_destruct(m);
+    fclose(f);
+    return -1;
+  }
+  *(Matrix2d**)result = m;
+
+  fclose(f);
+  return 0;
+}
 
 // Grade "Excellent"
 Queue* rand_gen_matrix2d_in_queue(size_t n) {
--- sem2/04-file/fioqueue.h
+++ sem2/04-file/fioqueue.h
@@ -6,24 +6,22 @@
 #include <stddef.h> // size_t
 
 // for generators
-#define MAX 100
-#define MIN -100
+#define MAX_DOUBLE 10000
+#define MIN_DOUBLE -10000
 
-// additional
-static inline int get_rand_int(int min, int max) 
-  return (int)rand() / (RAND_MAX + 1.0) * (max - min) + min;
-
+#define MAX_ROW 100
+#define MAX_COL 100 
 
 // Grade "Satisfactory"
 Matrix2d* rand_gen_matrix2d(size_t);
-int save_queue_to(const Queue*, const char*);
-int load_queue_from(Queue*, const char*);
+int save_text_queue_to(const Queue*, const char*);
+int load_text_queue_from(Queue*, const char*);
 Matrix2d* get_text_element(const char*, int); // get by index
 
 // Grade "Good"
-int saveb_queue_to(const Queue*, const char*);
-int loadb_queue_from(Queue*, const char*);
-int get_binary_element(const char*, int, size_t, void*); // get by index
+int save_binary_queue_to(const Queue*, const char*);
+int load_binary_queue_from(Queue*, const char*);
+int get_binary_element(const char*, int, void*); // get by index
 
 // Grade "Excellent"
 Queue* rand_gen_matrix2d_in_queue(size_t);
