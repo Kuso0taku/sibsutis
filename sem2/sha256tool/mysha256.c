69b6cc09cf44e6aba48ebc7a16db42ef44cd9b1d
Kuso0taku <denustuzan@gmail.com>
2026-05-14 19:11:49 +0700

    sha256tool const prime + lil statics

--- sem2/sha256tool/mysha256.c
+++ sem2/sha256tool/mysha256.c
@@ -14,3 +14,32 @@
 #define sigma0(x) (ROTR(x,  7) ^ ROTR(x, 18) ^ ((x) >> 3))
 #define sigma1(x) (ROTR(x, 17) ^ ROTR(x, 19) ^ ((x) >> 10))
 
+// first 64 prime numbers (for K)
+static const unsigned uint16_t prime_64[] = {
+  2,   3,   5,   7,   11,  13,  17,  19,
+  23,  29,  31,  37,  41,  43,  47,  53,
+  59,  61,  67,  71,  73,  79,  83,  89,
+  97,  101, 103, 107, 109, 113, 127, 131,
+  137, 139, 149, 151, 157, 163, 167, 173,
+  179, 181, 191, 193, 197, 199, 211, 223,
+  227, 229, 233, 239, 241, 251, 257, 263,
+  269, 271, 277, 281, 283, 293, 307, 311
+}
+
+// get first 32bits of fractional part
+static uint32_t fractional_bits(double value) {
+  double int;
+  double frac = modf(value, &int); // int = int part, frac = frac part
+  double scaled = frac * pow(2, 32); // get first 32 bits 
+  return (uint32_t)scaled;
+}
+
+// calculate init values of hash
+static void calc_init_hash(uint32_t H[8]) {
+  for (size_t i=0; i<8; i++) *(H+i) = fractional_bits(sqrt((double)*(primes_64+i)));
+}
+
+// calculate round constants
+static void calc_init_hash(uint32_t K[64]) {
+  for (size_t i=0; i<64; i++) *(K+i) = fractional_bits(sqrt((double)*(primes_64+i)));
+}
